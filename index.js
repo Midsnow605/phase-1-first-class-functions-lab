33fcf24f2c44a2ea47f7d7a619ffc60d3b9358dc
Jose <gutierrez.jose24@gmail.com>
2023-04-11 08:28:27 -0500

    phase 1 lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0, -2);
+}
+function returnLastTwoDrivers (drivers) {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(){
+    return function(x){return (x * x)};
+}
+function fareDoubler(x){
+    return x * 2;
+}
+function fareTripler(x){
+    return x * 3;
+}
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    let result;
+    if (selectingDrivers === returnFirstTwoDrivers) {
+      result = drivers.slice(0, 2);
+    } else if (selectingDrivers === returnLastTwoDrivers) {
+      result = drivers.slice(-2);
+    }
+    return result;
+  }
